4b80c4c176e08f9c6e05e92e08bd76bb187c9031
Will Dixon <dixonwille@hotmail.com>
2017-04-18 16:34:32 -0400

    Updated Windows install removing interval

--- WindowsInstall.ps1
+++ WindowsInstall.ps1
@@ -3,7 +3,6 @@ $TaskName = 'BusyBee'
 if (Get-ScheduledTask -TaskName $TaskName) {
     exit
 }
-$Interval = (New-TimeSpan -Minutes 5)
 $Action = New-ScheduledTaskAction -Execute "C:/Path/To/Exe" -Argument "-any args -passed here"
 $Trigger = New-ScheduledTaskTrigger -AtLogOn -User $User
 $Settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable
