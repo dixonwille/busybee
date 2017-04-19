924a53fd81da8a41cec1287c0e39cee73b647c34
Will Dixon <dixonwille@gmail.com>
2017-04-18 22:33:29 -0400

    Updated the Unix install to prompt for all variables needed
    Recommended to Remove the variables common for work and code them in

--- UnixInstall.sh
+++ UnixInstall.sh
@@ -1,4 +1,48 @@
 #!/bin/sh
-if ! crontab -l | grep -q '/path/to/job'; then
-    (crontab -l 2>/dev/null; echo "*/5 * * * * /path/to/job -with args") | crontab -
+BBPATH="$PWD/BusyBee"
+while [ ! -f $BBPATH ]
+do
+read -p "Location of BusyBee (FullPath): " BBPATH
+done
+
+while [ -z "$EXHOST" ]
+do
+read -p "Exchange Host: " EXHOST
+done
+
+while [ -z "$EXEMAIL" ]
+do
+read -p "Exchange Email: " EXEMAIL
+done
+
+while [ -z "$EXUSER" ]
+do
+read -p "Exchange UserName: " EXUSER 
+done
+
+while [ -z "$EXPASS" ]
+do
+read -s -p "Exchange PassWord: " EXPASS
+echo ""
+done
+
+while [ -z "$HCHOST" ]
+do
+read -p "HipChat Host: " HCHOST
+done
+
+while [ -z "$HCMENTION" ]
+do
+read -p "HipChat Mention: " HCMENTION
+done
+
+while [ -z "$HCTOKEN" ]
+do
+read -p "HipChat Token( $HCHOST/account/api ): " HCTOKEN
+done
+
+ARGS="-exHost '$EXHOST' -exUser '$EXUSER' -exPass '$EXPASS' -exUID '$EXEMAIL' -hcHost '$HCHOST' -hcToken '$HCTOKEN' -hcUID '$HCMENTION'"
+
+if ! crontab -l | grep -q "$BBPATH"; then
+   (crontab -l 2>/dev/null; echo "*/5 * * * * $BBPATH $ARGS") | crontab -
 fi
