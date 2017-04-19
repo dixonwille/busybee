979d5a78e9465c75b40ea338b99024c2e923ce3d
Will Dixon <dixonwille@hotmail.com>
2017-04-19 00:00:58 -0400

    Updated Windows Install script to test that arguments work

--- WindowsInstall.ps1
+++ WindowsInstall.ps1
@@ -1,9 +1,54 @@
 $User = $(whoami)
 $TaskName = 'BusyBee'
-if (Get-ScheduledTask -TaskName $TaskName) {
+if (Get-ScheduledTask -TaskName $TaskName -ErrorAction 'Ignore') {
     exit
 }
-$Action = New-ScheduledTaskAction -Execute "C:/Path/To/Exe" -Argument "-any args -passed here"
+
+$BBPath = "$PSScriptRoot/BusyBee.exe"
+while(!(Test-Path $BBPath)){
+    $BBPath = Read-Host -Prompt 'Location of BusyBee.exe (FullPath)'
+}
+
+while([string]::IsNullOrEmpty($ExHost)){
+    $ExHost = Read-Host -Prompt 'Exchange Host'
+}
+
+while([string]::IsNullOrEmpty($ExEmail)){
+    $ExEmail = Read-Host -Prompt 'Exchange Email'
+}
+
+while([string]::IsNullOrEmpty($ExUser)){
+    $ExUser = Read-Host -Prompt 'Exchange UserName'
+}
+
+while([string]::IsNullOrEmpty($ExPass)){
+    $ExPass = Read-Host -Prompt 'Exchange PassWord' -AsSecureString
+}
+
+while([string]::IsNullOrEmpty($HcHost)){
+    $HcHost = Read-Host -Prompt 'Hipchat Host'
+}
+
+while([string]::IsNullOrEmpty($HcMention)){
+    $HcMention = Read-Host -Prompt 'Hipchat Mention @'
+}
+
+while([string]::IsNullOrEmpty($HcToken)){
+    $HcToken = Read-Host -Prompt "Hipchat Token ( $HcHost/account/api )"
+}
+
+$RawPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($ExPass))
+
+$Args = "-exHost `"$ExHost`" -exUser `"$ExUser`" -exPass `"$RawPass`" -exUID `"$ExEmail`" -hcHost `"$HcHost`" -hcToken `"$HcToken`" -hcUID `"$HcMention`""
+
+$Test = (Start-Process -FilePath $BBPath -ArgumentList ($Args -split ' ') -WindowStyle Hidden -PassThru -Wait -RedirectStandardError 'blah.txt')
+
+if($Test.ExitCode -gt 0){
+    Write-Error 'Passed in Arguments did not allow BusyBee to execute properly. Please run install again.'
+    exit
+}
+
+$Action = New-ScheduledTaskAction -Execute $BBPath -Argument $Args
 $Trigger = New-ScheduledTaskTrigger -AtLogOn -User $User
 $Settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
